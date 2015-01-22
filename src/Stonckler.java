e6129d5cf14ec6857818b874fe6b8f89bd9cf0ea
kfrank15 <kfrank15@kentdenver.org>
2015-01-22 11:19:11 -0700

    Update Stonckler.java

--- src/Stonckler.java
+++ src/Stonckler.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
@@ -7,8 +8,8 @@ public class Stonckler {
 
 	public static void main(String[] args) throws IOException {
 		HashMap<String, Stock> stocks = new HashMap<String, Stock>();
-		// stocks.put("LuLuLemon", new Stock("LULU"));
-		// stocks.put("Tesla", new Stock("TSLA"));
+		stocks.put("LuLuLemon", new Stock("LULU"));
+		stocks.put("Tesla", new Stock("TSLA"));
 		stocks.put("Southwest", new Stock("LUV"));
 		stocks.put("Apple", new Stock("AAPL"));
 		stocks.put("Microsoft", new Stock("MSFT"));
@@ -18,27 +19,27 @@ public class Stonckler {
 		// stocks.put("Alibaba", new Stock("BABA"));
 		stocks.put("Oracle", new Stock("ORCL"));
 		stocks.put("Starbucks", new Stock("SBUX"));
-		stocks.put("Coca Cola", new Stock("KO"));
+
+		
+			FileWriter f=new FileWriter("data.csv", true);
+
+//			f.append("Stock Symbol"); f.append(','); f.append("Price"); f.append(','); f.append("Date"); f.append('\n');
+		
+			f.append('\n');
 		for (Entry<String, Stock> e : stocks.entrySet()) {
-			e.getValue().updatePrice();
-			System.out.println(e.getKey() + " " + e.getValue().getPrice());
-			StockValue.buyStock(e.getValue().getSymbol(), e.getValue().getPrice());
+//			e.getValue().updatePrice();
+//			System.out.println("Current price: "+e.getKey() + " " + e.getValue().getPrice());
+//
+//			f.write(e.getKey()+" "+e.getValue().getPrice()+"\n");
+//			System.out.println("Timestamp-"+e.getKey()+": "+e.getValue().getTime()+"\n");
+
+			f.append(e.getKey());
+			f.append(',');
+			f.append(""+e.getValue().getPrice());
+			f.append(',');
+			f.append(""+e.getValue().getTime());
+			f.append('\n');
 		}
-
-		FileWriter f = new FileWriter("data.txt", true);
-
-		for (Entry<String, Stock> e : stocks.entrySet()) {
-			Stock value = e.getValue();
-			value.updatePrice();
-			System.out.println("Current price: " + e.getKey() + " " + value.getPrice());
-
-			f.write(e.getKey() + " " + value.getPrice() + "\n");
-			f.write("Bought? " + StockValue.buyStock(value.getSymbol(), value.getPrice()) + "\n");
-			System.out.println("Timestamp-" + e.getKey() + ": " + value.getTime() + "\n");
-			f.write("Timestamp-" + e.getKey() + ": " + value.getTime() + "\n");
-			f.write("\n");
-		}
-		f.write("\n");
 		f.close();
 	}
 }
