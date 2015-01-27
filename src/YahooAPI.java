1f5b3d51a3b87ff376d6480089a4da1d9934b0da
MrClement <aclement@kentdenver.org>
2015-01-27 08:37:28 -0700

    Added historical data

--- src/YahooAPI.java
+++ src/YahooAPI.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 import org.json.JSONObject;
 
@@ -65,7 +66,19 @@ public class YahooAPI {
 				quote = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D'"
 						+ symbol
 						+ "'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
-
+			} else if (source.equals("historical")) {
+				String startDate = "2012-01-01";
+				GregorianCalendar now = new GregorianCalendar();
+				String endDate = now.get(GregorianCalendar.YEAR) + "-" + now.get(GregorianCalendar.MONTH) + "-"
+						+ now.get(GregorianCalendar.DAY_OF_MONTH);
+				quote = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22"
+						+ symbol
+						+ "%22%20and%20startDate%20%3D%20%22"
+						+ startDate
+						+ "%22%20and%20endDate%20%3D%20%22"
+						+ endDate
+						+ "%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+				System.out.println(quote);
 			} else {
 				quote = "";
 			}
