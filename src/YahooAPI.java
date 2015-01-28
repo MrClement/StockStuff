5f935f5a1325a8438408bce3f5732c93eda208a0
MrClement <aclement@kentdenver.org>
2015-01-28 14:58:16 -0700

    change

--- src/YahooAPI.java
+++ src/YahooAPI.java
@@ -82,7 +82,6 @@ public class YahooAPI {
 						+ "%22%20and%20endDate%20%3D%20%22"
 						+ endDate
 						+ "%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
-				System.out.println(quote);
 			} else {
 				quote = "";
 			}
